6eb99a86d9b97840806ff181288f7ab8fbd26d19
Miguel <mbermudez@itpfp.com>
2023-10-05 11:23:12 +0200

    hola mundo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.sendFile(path.resolver(__dirname, 'Taller-coches/index.html'))
+  res.sendFile((__dirname, 'hola miundo'))
 })
 
 
